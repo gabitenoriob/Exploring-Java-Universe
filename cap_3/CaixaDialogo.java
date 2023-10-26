5d4834c30605daf1b2b85ebad7d425e7273a7f70
gabitenoriob <gabitenoriob@gmail.com>
2023-10-25 21:42:21 -0300

    cap 3 ok

--- cap_3/CaixaDialogo.java
+++ cap_3/CaixaDialogo.java
@@ -1,5 +1,30 @@
 package cap_3;
+import javax.print.attribute.standard.JobHoldUntil;
+import javax.swing.JOptionPane; 
 
-public class CaixaDialogo {
-    
-}
+public class CaixaDialogo
+{
+   public static void main(String[] args)
+   {
+      // display a dialog with a message 
+      JOptionPane.showMessageDialog(null, "Welcome to Java");
+
+      // prompt user to enter name
+      String name = JOptionPane.showInputDialog("What is your name?");
+      
+      // create the message
+      String message =                                              
+         String.format("Welcome, %s, to Java Programming!", name);
+
+      // display the message to welcome the user by name 
+      JOptionPane.showMessageDialog(null, message);
+
+      String ageString = JOptionPane.showInputDialog("Qual sua idade?");
+      int age = Integer.parseInt(ageString);
+
+      String messageAge = String.format("Sua idade é : %d%n", age);
+      JOptionPane.showMessageDialog(null, messageAge);
+
+   
+   } 
+} // end class Dialog1
